86ff58b2befa514d28a1e6cb2843ea13cfb81ac8
therajusah <workwithrajukumar@gmail.com>
2024-04-12 18:07:43 +0530

    added react hot toast

--- LMS-frontend/src/App.jsx
+++ LMS-frontend/src/App.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+useEffect(() => {
+  toast.success('hello');
+})
+
+
   return (
     <h1 className="text-3xl font-bold ">
             Hello world!
