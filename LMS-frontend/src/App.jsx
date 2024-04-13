2973280c0ee5f08018e277a89835b516b491ab36
therajusah <workwithrajukumar@gmail.com>
2024-04-13 23:22:15 +0530

    redux setup with toolkit done

--- LMS-frontend/src/App.jsx
+++ LMS-frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { useEffect } from 'react';
-import './App.css';
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import { useEffect } from "react";
+import { toast } from "react-toastify"; 
+
 
 const App = () => {
-useEffect(() => {
-  toast.success('hello');
-})
+  useEffect(() => {
+    toast.success("Welcome");
+  }, []); 
 
+  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+};
 
-  return (
-    <h1 className="text-3xl font-bold ">
-            Hello world!
-          </h1>
-  )
-}
-
-export default App
+export default App;
--- LMS-frontend/src/main.jsx
+++ LMS-frontend/src/main.jsx
@@ -1,14 +1,18 @@
 import "./index.css";
 
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
+import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-    <Toaster />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 );
