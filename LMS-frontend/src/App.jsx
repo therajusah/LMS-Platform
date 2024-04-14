bd28d7b284e072b3e62657de63cb7a9a9dee71c8
therajusah <workwithrajukumar@gmail.com>
2024-04-14 23:39:51 +0530

    added daisy UI (in config file) & Footer

--- LMS-frontend/src/App.jsx
+++ LMS-frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
-import "react-toastify/dist/ReactToastify.css";
-
-import { useEffect } from "react";
-import { toast } from "react-toastify"; 
-
 
 const App = () => {
-  useEffect(() => {
-    toast.success("Welcome");
-  }, []); 
-
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+ 
+  return (
+  <h1 className="text-3xl font-bold">
+    Hello world!
+    </h1>
+  
+)
 };
 
 export default App;
