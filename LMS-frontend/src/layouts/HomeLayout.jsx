568e4c6ed933fcd3ea28987f65dd0bf4db1017aa
therajusah <workwithrajukumar@gmail.com>
2024-04-19 09:22:27 +0530

    added homeLayout to homepage

--- LMS-frontend/src/layouts/HomeLayout.jsx
+++ LMS-frontend/src/layouts/HomeLayout.jsx
@@ -1,45 +1,60 @@
+import { AiFillCloseCircle } from 'react-icons/ai';
 import { FiMenu } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-import Footer from "../components/Footer"
+import Footer from '../components/Footer';
 
 const HomeLayout = ({ children }) => {
 
-function changeWidth() {
-    const drawerSize = document.getElementsByClassName("drawer-side");
-drawerSize[0].style.width = 'auto';
+  function changeWidth() {
+    const drawerSide = document.getElementsByClassName("drawer-side");
+    drawerSide[0].style.width = 'auto';
+  }
+  function hideDrawer() {
+    const element = document.getElementsByClassName("drawer-toggle");
+    element[0].checked = false;
+    
+    const drawerSide = document.getElementsByClassName("drawer-side");
+    drawerSide[0].style.width = '0';
+  }
 
-}
 
   return (
     <div className="min-h-[90vh]">
-<div className="drawer absolute left-0 z-50 w-full">
-<input id="my-drawer" type="checkbox" className="drawer-toggle" />
-<div className="drawer-content">
-    <label htmlFor="my-drawer">
-        <FiMenu onClick='{changeWidth}' size={"32px"} className='font-bold text-white m-4'/>
-    </label>
-</div>
-<div className="drawer-side w-0">
-<label htmlFor="my-drawer" className="drawer-overlay"></label>
-<ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative"> 
-<li>
-    <Link to="/">Home</Link>
-</li>
-<li>
-    <Link to="/allcourses">All Courses</Link>
-</li>
-<li>
-    <Link to="/contact">Contact us</Link>
-</li>
-<li>
-    <Link to="/about">About us</Link>
-</li>
-
-</ul>
-</div>
-</div>
-</div>
+      <div className="drawer absolute left-0 z-50 w-full">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <label htmlFor="my-drawer">
+            <FiMenu onClick={changeWidth} size={"32px"} className='font-bold text-white m-4'/>
+          </label>
+        </div>
+        <div className="drawer-side w-0">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative"> 
+          <li className='w-fit absolute right-2 z-50 drawerClose'>
+            <button onClick={hideDrawer}>
+                <AiFillCloseCircle size={24} />
+            </button>
+          </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/allcourses">All Courses</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact us</Link>
+            </li>
+            <li>
+              <Link to="/about">About us</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {children}
+      <Footer />
+    </div>
   );
 }
 
-export default HomeLayout
+export default HomeLayout;
--- LMS-frontend/src/pages/Home.jsx
+++ LMS-frontend/src/pages/Home.jsx
@@ -1,13 +1,40 @@
+import { Link } from "react-router-dom";
 
-import HomeLayout from '../layouts/HomeLayout';
+import homePageMainImage  from "../assets/Images/homePageMainImage.png";
+import HomeLayout from "../layouts/HomeLayout";
 
 const Home = () => {
   return (
     <HomeLayout>
-    <div>Hey How are you?</div>
+      <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
+        <div className="w-1/2 space-y-6">
+          <h1 className="text-5xl font-semibold">
+            Find out best{" "}
+            <span className="text-blue-500 font-bold">Online courses</span>
+          </h1>
+          <p className="text-xl text-gray-200">
+            we have a large library of courses taught by highly skilled and
+            qualified faculties at best prices
+          </p>
+          <div className="space-x-6">
+            <Link to="/courses">
+              <button className="bg-blue-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-blue-600 transition-all ease-in-out duration-300">
+                Browse Courses
+              </button>
+            </Link>
+            <Link to="/contacts">
+              <button className="border border-blue-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-blue-600 transition-all ease-in-out duration-300">
+                Contact us
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="w-1/2 flex items-center justify-center">
+          <img src={homePageMainImage} alt="home page" />
+        </div>
+      </div>
     </HomeLayout>
-
-  )
-}
+  );
+};
 
 export default Home;
