88648c7d050da630d24369620d38bc0530c96451
therajusah <workwithrajukumar@gmail.com>
2024-04-23 22:42:23 +0530

    "Added conditional rendering for login and logout buttons based on user authentication status

--- LMS-frontend/src/layouts/HomeLayout.jsx
+++ LMS-frontend/src/layouts/HomeLayout.jsx
@@ -1,44 +1,67 @@
-import { AiFillCloseCircle } from 'react-icons/ai';
-import { FiMenu } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { AiFillCloseCircle } from "react-icons/ai";
+import { FiMenu } from "react-icons/fi";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 
 const HomeLayout = ({ children }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+  const role = useSelector((state) => state?.auth?.role);
 
   function changeWidth() {
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = 'auto';
+    drawerSide[0].style.width = "auto";
   }
   function hideDrawer() {
     const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
-    
+
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = '0';
+    drawerSide[0].style.width = "0";
   }
 
+  function onLogout() {
+    e.preventDefault();
+
+    //todo
+    navigate("/");
+  }
 
   return (
     <div className="min-h-[90vh]">
-      <div className="drawer absolute left-0 z-50 w-full">
+      <div className="absolute left-0 z-50 w-full drawer">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           <label htmlFor="my-drawer">
-            <FiMenu onClick={changeWidth} size={"32px"} className='font-bold text-white m-4'/>
+            <FiMenu
+              onClick={changeWidth}
+              size={"32px"}
+              className="m-4 font-bold text-white"
+            />
           </label>
         </div>
-        <div className="drawer-side w-0">
+        <div className="w-0 drawer-side">
           <label htmlFor="my-drawer" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative"> 
-          <li className='w-fit absolute right-2 z-50 drawerClose'>
-            <button onClick={hideDrawer}>
+          <ul className="relative w-48 p-4 menu sm:w-80 bg-base-200 text-base-content">
+            <li className="absolute z-50 w-fit right-2 drawerClose">
+              <button onClick={hideDrawer}>
                 <AiFillCloseCircle size={24} />
-            </button>
-          </li>
+              </button>
+            </li>
             <li>
               <Link to="/">Home</Link>
             </li>
+            {
+              isLoggedIn && role === "ADMIN" && (
+                <li>
+                <link to="/admin/dashboard" >Admin Dashboard</link>
+                </li>
+              )
+            }
             <li>
               <Link to="/allcourses">All Courses</Link>
             </li>
@@ -55,6 +78,6 @@ const HomeLayout = ({ children }) => {
       <Footer />
     </div>
   );
-}
+};
 
 export default HomeLayout;
