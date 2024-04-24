b46d916068b087be82abbb172ba9024d039b9b6b
therajusah <workwithrajukumar@gmail.com>
2024-04-24 11:25:05 +0530

    "Add Aboutus route to the app"
    This commit adds a new route for the Aboutus page in the application. The route path is "/about" and the component rendered is <Aboutus/>.

--- LMS-frontend/src/App.jsx
+++ LMS-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 
+import Aboutus from "./pages/Aboutus";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<Aboutus/>} />
     </Routes>
   );
 };
---
+++ LMS-frontend/src/pages/Aboutus.jsx
@@ -0,0 +1,87 @@
+import aboutMainPage from "../assets/Images/aboutMainImage.png";
+import apj from "../assets/Images/apj.png";
+import einstein from "../assets/Images/einstein.png";
+import nelsonMandela from "../assets/Images/nelsonMandela.png";
+import steveJobs from "../assets/Images/steveJobs.png";
+import HomeLayout from "../layouts/HomeLayout";
+
+function Aboutus() {
+  return (
+    <HomeLayout>
+      <div className="flex flex-col pt-10 pl-20 text-white">
+        <div className="flex items-center gap-5 mx-10">
+          <section className="w-1/2 spacing-y-10">
+            <h1 className="pb-10 text-5xl font-semibold text-blue-500">
+              Affordable and quality education
+            </h1>
+            <p className="text-xl text-gray-200">
+              Discover quality education at affordable rates on our platform. We
+              are committed to making learning accessible to all, offering
+              top-notch courses without the hefty price tag. Our curated
+              selection ensures you receive value without compromise.
+            </p>
+          </section>
+          <div className="w-1/2">
+            <img
+              src={aboutMainPage}
+              className="drop-shadow-2xl"
+              alt="about main page"
+            />
+          </div>
+        </div>
+        <div className="w-1/2 mx-auto my-10 carousel">
+          <div id="slide1" className="relative w-full carousel-item">
+            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+              <img src={apj} className="w-40 border-2 border-gray-400 rounded-full " />
+              <p className="text-xl text-gray-200">Excellence is a continuous process and not an accident.</p>
+              <h3 className="text-2xl font-semibold">APJ Abdul Kalam</h3>
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide4" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide2" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+          <div id="slide2" className="relative w-full carousel-item">
+            <img src={steveJobs} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide3" className="relative w-full carousel-item">
+            <img src={einstein} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide4" className="relative w-full carousel-item">
+            <img src={nelsonMandela} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </HomeLayout>
+  );
+}
+
+export default Aboutus;
